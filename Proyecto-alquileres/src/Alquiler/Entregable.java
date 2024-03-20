db24f26c012ef6524be8877cd235c99da57d8da2
Roshhz <davidarielmezaa@gmail.com>
2024-03-19 23:58:19 -0300

    commit 1

---
+++ Proyecto-alquileres/src/Alquiler/Entregable.java
@@ -0,0 +1,13 @@
+package Alquiler;
+
+public interface Entregable extends Comparable<Object> {
+	public void entregar();
+
+	public void devolver();
+
+	public Boolean isEntregado();
+
+	@Override
+	int compareTo(Object a);
+
+}
