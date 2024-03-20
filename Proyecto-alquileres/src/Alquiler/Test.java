4439b2f4fb81c69c7d3e06a7c9a7a9cd459cea37
Roshhz <davidarielmezaa@gmail.com>
2024-03-20 00:04:42 -0300

    commit

--- Proyecto-alquileres/src/Alquiler/Test.java
+++ Proyecto-alquileres/src/Alquiler/Test.java
@@ -3,7 +3,7 @@ package Alquiler;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Prueba {
+public class Test {
 
 	public static void main(String[] args) {
 		ArrayList<Pelicula> alquileresPeliculas = new ArrayList<>();
